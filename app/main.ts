565880f30442d07dd06e8d3ea11d6a386233fa70
Abd Essamed <abdessamed630@gmail.com>
2025-07-30 15:28:48 +0100

    Implement the ECHO command

--- app/main.ts
+++ app/main.ts
@@ -9,9 +9,9 @@ console.log("Logs from your program will appear here!");
    // Handle connection
    connection.on('data', (data : Buffer)=>{
      console.log(`Received data from  redis client :${data}`);
-     const message: String = redisParser(data)
+     const message: string = redisParser(data)
       if (message) {
-          connection.write(JSON.stringify(message));
+          connection.write(message);
       }else {
           connection.write('+PONG\r\n')
       }
@@ -21,18 +21,17 @@ console.log("Logs from your program will appear here!");
   })
  });
 
- const redisParser  = (command : Buffer) : String=>{
+ const redisParser  = (command : Buffer) : string=>{
 
      let cmd_arr : String[] = command.toString().split('\r\n');
      cmd_arr = cmd_arr.filter((_, index) => index % 2 !== 0);
-     let message:String ='';
+     let message:string ='';
      if(cmd_arr.length > 0 && cmd_arr[0].toUpperCase() == 'ECHO'){
          for(let i = 1; i < cmd_arr.length; i++){
              const bytes = cmd_arr[i].length;
              message += `$${bytes}\r\n${cmd_arr[i]}\r\n`;
          }
      }
-     console.log(message);
      return message
  }
  server.listen(6379, "127.0.0.1");
