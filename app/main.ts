2346c326d97b1149aef94ef2e8c505ca4eaaa76f
Abd Essamed <abdessamed630@gmail.com>
2025-07-30 12:23:48 +0100

    handling PING response

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
  const server: net.Server = net.createServer((connection: net.Socket) => {
+   connection.setEncoding('utf8');
    // Handle connection
+   connection.on('data', (data)=>{
+     console.log(`Received data from  redis client :${data}`);
+
+     connection.write('+PONG\\r\\n')
+   })
+  connection.on('end', ()=>{
+    console.log('Client disconnected');
+  })
  });
 
  server.listen(6379, "127.0.0.1");
