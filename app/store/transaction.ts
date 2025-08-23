ccdad8912f5c285c63b3269d170dda94a506d379
Abd Essamed <abdessamed630@gmail.com>
2025-08-23 20:44:42 +0100

    codecrafters submit [skip ci]

--- app/store/transaction.ts
+++ app/store/transaction.ts
@@ -5,7 +5,7 @@ export class transactionStore {
 
   incr(key: string, by: number = 1): number {
     const value = this.stringStore.get(key);
-
+    if(value && Number.isNaN(Number(value))) throw new Error('value is not an integer or out of range')
     const result = value ? Number(value) + by : by;
     this.stringStore.set(key, String(result));
     return result;
